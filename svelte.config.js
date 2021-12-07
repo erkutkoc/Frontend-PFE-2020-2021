5840dad357bbff2c33f2d0831d5f3f5a1ad8c580
Hosam Mohamed <hosam.mohamed@student.vinci.be>
2021-12-07 11:53:04 +0100

    Add - typescript integration + Tailwind/Bulma CSS dependencies integration + Login page layout + Routes

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-const preprocess = require('svelte-preprocess');
+import preprocess from 'svelte-preprocess';
 
-module.exports = {
-    disableDependencyReinclusion: ['@roxi/routify'],
-
-    preprocess: [
-        preprocess(),
-    ],
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+	],
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
+
+export default config;
